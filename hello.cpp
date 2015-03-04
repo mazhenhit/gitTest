a4a1e4e576cd2e79d0c5246b2748a126b85df568
mazhenhit <mazhenhit@foxmail.com>
2015-03-04 20:04:57 +0800

    remove git info

--- hello.cpp
+++ hello.cpp
@@ -4,10 +4,7 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 		cout << " Createing a new branch is quick simple." << endl;
-=======
 		cout << " Createing a new branch is quick AND simple." << endl;
->>>>>>> feature1
 		return 0;
 }
